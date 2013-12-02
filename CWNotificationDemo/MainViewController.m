2185bb8b8552f67c5c846d8079b61f1cf783098a
Collin Ruffenach <collin@simple.com>
2013-12-01 19:54:09 -0800

    Respect settings for text alignment, spring physics etc

--- CWNotificationDemo/MainViewController.m
+++ CWNotificationDemo/MainViewController.m
@@ -10,16 +10,30 @@
 #import "CWStatusBarNotification.h"
 
 @interface MainViewController ()
+
+@property (weak, readonly) NSDictionary *options;
+
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 @property (weak, nonatomic) IBOutlet UIView *contentView;
-@property (weak, nonatomic) IBOutlet UILabel *lblDuration;
-@property (weak, nonatomic) IBOutlet UISlider *sliderDuration;
-@property (weak, nonatomic) IBOutlet UITextField *txtNotificationMessage;
+
 @property (weak, nonatomic) IBOutlet UISegmentedControl *segFromStyle;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *segToStyle;
+
+@property (weak, nonatomic) IBOutlet UISlider *sliderDuration;
+@property (weak, nonatomic) IBOutlet UILabel *lblDuration;
+
 @property (weak, nonatomic) IBOutlet UISwitch *showImageSwitch;
 @property (weak, nonatomic) IBOutlet UISwitch *coverNavBarSwitch;
+@property (weak, nonatomic) IBOutlet UISwitch *springPhysicsSwitch;
+@property (weak, nonatomic) IBOutlet UISwitch *slideOverSwitch;
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segAlignment;
+
+@property (weak, nonatomic) IBOutlet UITextField *txtNotificationMessage;
 @property (weak, nonatomic) IBOutlet UIButton *showNotificationButton;
+
+@property (assign, nonatomic) NSTextAlignment textAlignment;
+
 @end
 
 @implementation MainViewController
@@ -64,22 +78,10 @@
     [self updateDurationLabel];
 }
 
-# pragma mark - show notification
+# pragma mark - Show Notification
 
 - (IBAction)btnShowNotificationPressed:(UIButton *)sender {
-    NSMutableDictionary *options = [@{kCWStatusBarNotificationNotificationTypeKey               : self.coverNavBarSwitch.on ? @(CWStatusBarNotificationTypeNavigationBar) : @(CWStatusBarNotificationTypeStatusBar),
-                                      kCWStatusBarNotificationAnimationTypeKey                  : @(CWStatusBarNotificationAnimationTypeSpring),
-                                      kCWStatusBarNotificationAnimateInTimeIntervalKey          : @(0.5),
-                                      kCWStatusBarNotificationAnimateOutTimeIntervalKey         : @(0.5),
-                                      kCWStatusBarNotificationTextKey                           : self.txtNotificationMessage.text,
-                                      kCWStatusBarNotificationTimeIntervalKey                   : @(self.sliderDuration.value),
-                                      kCWStatusBarNotificationNotificationInAnimationStyleKey   : @(self.segFromStyle.selectedSegmentIndex),
-                                      kCWStatusBarNotificationNotificationOutAnimationStyleKey  : @(self.segToStyle.selectedSegmentIndex)} mutableCopy];
-    if (self.showImageSwitch.on) {
-        options[kCWStatusBarNotificationImageKey] = [UIImage imageNamed:@"alert_icon.png"];
-    }
-    
-    [CWStatusBarNotificationManager showNotificationWithOptions:[NSDictionary dictionaryWithDictionary:options]
+    [CWStatusBarNotificationManager showNotificationWithOptions:[self options]
                                                 completionBlock:^{
                                                     NSLog(@"Completed");
                                                 }];
@@ -95,6 +97,35 @@
     self.scrollView.scrollIndicatorInsets = self.scrollView.contentInset;
     self.scrollView.contentSize = CGSizeMake(CGRectGetWidth(self.contentView.frame),
                                              CGRectGetMaxY(self.showNotificationButton.frame));
+}
+
+#pragma mark - Overrides
+
+- (NSDictionary*)options {
+    NSMutableDictionary *options = [@{kCWStatusBarNotificationNotificationTypeKey               : self.coverNavBarSwitch.on ? @(CWStatusBarNotificationTypeNavigationBar) : @(CWStatusBarNotificationTypeStatusBar),
+                                      kCWStatusBarNotificationTextKey                           : self.txtNotificationMessage.text,
+                                      kCWStatusBarNotificationTextAlignmentKey                  : @(self.textAlignment),
+                                      kCWStatusBarNotificationTimeIntervalKey                   : @(self.sliderDuration.value),
+                                      kCWStatusBarNotificationNotificationInAnimationStyleKey   : @(self.segFromStyle.selectedSegmentIndex),
+                                      kCWStatusBarNotificationNotificationOutAnimationStyleKey  : @(self.segToStyle.selectedSegmentIndex)} mutableCopy];
+    if (self.showImageSwitch.on) {
+        options[kCWStatusBarNotificationImageKey] = [UIImage imageNamed:@"alert_icon.png"];
+    }
+    
+    if (self.springPhysicsSwitch.on) {
+        options[kCWStatusBarNotificationAnimationTypeKey] = @(CWStatusBarNotificationAnimationTypeSpring);
+        options[kCWStatusBarNotificationAnimateInTimeIntervalKey] = @(0.5);
+        options[kCWStatusBarNotificationAnimateOutTimeIntervalKey] = @(0.5);
+    }
+    
+    return [NSDictionary dictionaryWithDictionary:options];
+}
+
+- (NSTextAlignment)textAlignment {
+    NSInteger selectedSegment = self.segAlignment.selectedSegmentIndex;
+    return selectedSegment == 0 ? NSTextAlignmentLeft :
+    selectedSegment == 1 ? NSTextAlignmentCenter :
+    NSTextAlignmentRight;
 }
 
 @end
