7942f64f8ddc53e20a4e6ac0afcd97450fc56ebc
Collin Ruffenach <collin@simple.com>
2014-03-11 11:27:09 -0700

    DRY app delegate

--- CRToastDemo/AppDelegate.m
+++ CRToastDemo/AppDelegate.m
@@ -14,14 +14,11 @@
                                                         kCRToastFontKey             : [UIFont fontWithName:@"HelveticaNeue-Light" size:16],
                                                         kCRToastTextColorKey        : [UIColor whiteColor],
                                                         kCRToastBackgroundColorKey  : [UIColor orangeColor]}];
-    
-    
+        
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor whiteColor];
-    MainViewController *mainViewController = [[MainViewController alloc] initWithNibName:@"MainViewController" bundle:nil];
-    UINavigationController *navigationController = [UINavigationController new];
+    UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:[[MainViewController alloc] initWithNibName:@"MainViewController" bundle:nil]];
     self.window.rootViewController = navigationController;
-    [navigationController pushViewController:mainViewController animated:NO];
     [self.window makeKeyAndVisible];
     return YES;
 }
