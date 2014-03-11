e79d8bef7abce48d10ab16b37dd2d8d346f1c79c
Ashton-W <Ashton-W@users.noreply.github.com>
2014-03-11 15:21:10 +1100

    added Travis CI cfg, and shared scheme removed old test target for CWNotifications, created one for CRToast

---
+++ CRToastTests/CRToastTests.m
@@ -0,0 +1,35 @@
+//
+//  CRToastTests.m
+//  CRToastTests
+//
+//  Created by Ashton Williams on 11/03/2014.
+//  Copyright (c) 2014 Collin Ruffenach. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "CRToast.h"
+
+@interface CRToastTests : XCTestCase
+
+@end
+
+@implementation CRToastTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testEmptyDefaultOptions
+{
+    [CRToastManager setDefaultOptions:@{}];
+}
+
+@end
