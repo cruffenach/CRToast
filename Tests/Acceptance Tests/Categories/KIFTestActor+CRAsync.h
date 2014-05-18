fd37cd39fb94a6ca928a8d89ef19b139adc0a766
Ashton-W <Ashton-W@users.noreply.github.com>
2014-05-18 16:28:53 +1000

    Added Acceptance Tests target. Moved Unit tests to `Tests\` Added development `Podfile` for KIF Acceptance Tests (Only for Test target). Added Device Orientation tests.

---
+++ Tests/Acceptance Tests/Categories/KIFTestActor+CRAsync.h
@@ -0,0 +1,13 @@
+//
+//  KIFTestActor+CRAsync.h
+//  CRToastDemo
+//
+//  Created by Ashton Williams on 16/05/2014.
+//  Copyright (c) 2014 Collin Ruffenach. All rights reserved.
+//
+
+#import "KIFTestActor.h"
+
+@interface KIFTestActor (CRAsync)
+
+@end
