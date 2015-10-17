87e310539acfdc2490ef8118c6fc1aa4f4b18c51
Ashton Williams <AJWilliams@odecee.com.au>
2015-10-17 16:36:59 +1100

    Removed #define from public header.
    Also inlined it since it was used in one place anyway.

--- CRToast/CRToast.h
+++ CRToast/CRToast.h
@@ -7,8 +7,6 @@
 #import "CRToastManager.h"
 #import <UIKit/UIKit.h>
 
-#define IS_OS_8_OR_LATER    ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0)
-
 @class CRToastSwipeGestureRecognizer, CRToastTapGestureRecognizer;
 
 /**
--- CRToast/CRToastLayoutHelpers.h
+++ CRToast/CRToastLayoutHelpers.h
@@ -205,7 +205,7 @@ static CGRect CRGetNotificationContainerFrame(UIInterfaceOrientation statusBarOr
 
 /// Get view snapshot. If `underStatusBar` it will get key windows root view controller. Otherwise it'll get the mainscreens snapshot
 static UIView *CRStatusBarSnapShotView(BOOL underStatusBar) {
-    if(IS_OS_8_OR_LATER){
+    if([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0) {
         return underStatusBar ?
         [[UIApplication sharedApplication].keyWindow.rootViewController.view snapshotViewAfterScreenUpdates:YES] :
         [[UIScreen mainScreen] snapshotViewAfterScreenUpdates:YES];
