2b16c5d5d7cfbdfecb54c4f726972a7790597901
Ashton Williams <Ashton-W@users.noreply.github.com>
2015-10-20 22:02:30 +1100

    fixed name of project in framework header constant

--- CRToast/CRToast.h
+++ CRToast/CRToast.h
@@ -8,7 +8,7 @@
 //! Project version number for CRToast.
 FOUNDATION_EXPORT double CRToastVersionNumber;
 
-//! Project version string for CRToastCarthage.
+//! Project version string for CRToast.
 FOUNDATION_EXPORT const unsigned char CRToastVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <CRToast/PublicHeader.h>
